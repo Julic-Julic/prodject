50ff546e750b9e0d1b5111aa9e8ea2f671462fbd
coco <uharina0@gmail.com>
2025-11-30 23:12:56 +0900

    Поменяла

--- src/swiper.js
+++ src/swiper.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const swiper = new Swiper(".swiper", {
     direction: "horizontal",
     loop: true,
-    slidesPerView: "auto", // Автоматическая ширина слайдов
-    spaceBetween: 24, // Отступ между слайдами
+    slidesPerView: "auto", 
+    spaceBetween: 24, 
     centeredSlides: false,
     pagination: {
       el: ".swiper-pagination",
